51a6b09a91b69288fe7ef01abb80b55f4bbeb46e
zyy7259 <zyy7259@gmail.com>
2014-09-03 21:28:25 +0800

    ...

--- js/index.js
+++ js/index.js
@@ -4,103 +4,237 @@
             this.initData();
             this.initNode();
             this.initEvent();
+            this.pullBlogList();
         },
         initData: function() {
             this.defaultBlogTitle = '日志标题';
-            this.defaultBlogCnt = '这里可以写日志哦~';
+            this.defaultBlogContent = '这里可以写日志哦~';
         },
         initNode: function() {
-            this.blogTab = $.get('blogTab');
-            this.blogTabCnt = $.get('blogTabCnt');
-            this.tagTab = $.get('tagTab');
-            this.tagTabCnt = $.get('tagTabCnt');
+            this.$blogTab = $.get('blogTab');
+            this.$blogTabCnt = $.get('blogTabCnt');
+            this.$tagTab = $.get('tagTab');
+            this.$tagTabCnt = $.get('tagTabCnt');
 
-            this.blogTitle = $.get('blogTitle');
-            this.blogCnt = $.get('blogCnt');
-            this.submitBlogBtn = $.get('submitBlog');
-            this.resetBlogBtn = $.get('resetBlog');
-            this.blogForm = $.get('blogForm');
+            this.$blogId = $.get('blogId');
+            this.$blogTitle = $.get('blogTitle');
+            this.$blogContent = $.get('blogContent');
+            this.$submitBlog = $.get('submitBlog');
+            this.$resetBlog = $.get('resetBlog');
+            this.$blogForm = $.get('blogForm');
 
-            this.blogList = $.get('blogList');
-            this.blogItmTmp = $.get('blogItmTmp');
+            this.$blogItmTmp = $.get('blogItmTmp');
+            this.$blogList = $.get('blogList');
+            this.$topBlogList = $.get('topBlogList');
 
-            this.checkAll = $.get('checkAll');
-            this.deleteAll = $.get('deleteAll');
+            this.$checkAll = $.get('checkAll');
+            this.$deleteAll = $.get('deleteAll');
         },
         initEvent: function() {
-            $.on(this.blogTab, 'click', this.switchToBlog.bind(this));
-            $.on(this.tagTab, 'click', this.switchToTag.bind(this));
+            $.on(this.$blogTab, 'click', this.switchToBlog.bind(this));
+            $.on(this.$tagTab, 'click', this.switchToTag.bind(this));
 
-            $.on(this.blogTitle, 'focus', this.focusBlogTitle.bind(this));
-            $.on(this.blogTitle, 'blur', this.blurBlogTitle.bind(this));
-            $.on(this.blogCnt, 'focus', this.focusBlogCnt.bind(this));
-            $.on(this.blogCnt, 'blur', this.blurBlogCnt.bind(this));
-            $.on(this.submitBlogBtn, 'click', this.submitBlog.bind(this));
-            $.on(this.resetBlogBtn, 'click', this.resetBlog.bind(this));
+            $.on(this.$blogTitle, 'focus', this.focusBlogTitle.bind(this));
+            $.on(this.$blogTitle, 'blur', this.blurBlogTitle.bind(this));
+            $.on(this.$blogContent, 'focus', this.focusBlogCnt.bind(this));
+            $.on(this.$blogContent, 'blur', this.blurBlogCnt.bind(this));
+            $.on(this.$submitBlog, 'click', this.submitBlog.bind(this));
+            $.on(this.$resetBlog, 'click', this.resetBlog.bind(this));
 
-            $.on(this.checkAll, 'click', this.checkAllBlog.bind(this));
-            $.on(this.deleteAll, 'click', this.deleteAllBlog.bind(this));
+            $.on(this.$blogList.parentNode, 'click', this.clickBlogList.bind(this));
+
+            $.on(this.$checkAll, 'click', this.checkAllBlog.bind(this));
+            $.on(this.$deleteAll, 'click', this.deleteAllBlog.bind(this));
         },
+
         switchToBlog: function(event) {
-            this.blogTab.parentNode.className = 'z-crt';
-            this.tagTab.parentNode.className = '';
-            this.blogTabCnt.style.display = '';
-            this.tagTabCnt.style.display = 'none';
+            this.$blogTab.parentNode.className = 'z-crt';
+            this.$tagTab.parentNode.className = '';
+            this.$blogTabCnt.style.display = '';
+            this.$tagTabCnt.style.display = 'none';
         },
         switchToTag: function(event) {
-            this.blogTab.parentNode.className = '';
-            this.tagTab.parentNode.className = 'z-crt';
-            this.blogTabCnt.style.display = 'none';
-            this.tagTabCnt.style.display = '';
+            this.$blogTab.parentNode.className = '';
+            this.$tagTab.parentNode.className = 'z-crt';
+            this.$blogTabCnt.style.display = 'none';
+            this.$tagTabCnt.style.display = '';
+        },
+
+        pullBlogList: function() {
+            $.rest({
+                url: 'http://fed.hz.netease.com/api/getblogs',
+                method: 'GET',
+                onload: this.cbBlogList.bind(this)
+            });
+        },
+        cbBlogList: function(json) {
+            var id = +new Date;
+            if (!!json) {
+                this.blogList = json;
+                this.blogList.sort(function(a, b) {return +b.modifyTime - +a.modifyTime});
+                for (var i = 0, l = this.blogList.length; i < l; i++) {
+                    var blog = this.blogList[i];
+                    blog.id = id++;
+                    this.blogList[blog.id] = blog;
+                    switch (blog.rank) {
+                        case "0":
+                        this.appendBlog(blog);
+                        break;
+                        case "5":
+                        this.appendTopBlog(blog);
+                        break;
+                    }
+                }
+            }
+            console.log(this.blogList);
+        },
+
+        appendTopBlog: function(blog) {
+            var $blog = this.cloneBlogItm(blog);
+            this.$topBlogList.appendChild($blog);
+        },
+        appendBlog: function(blog) {
+            var $blog = this.cloneBlogItm(blog);
+            this.$blogList.appendChild($blog);
+        },
+        insertBlog: function(blog) {
+            var $blog = this.cloneBlogItm(blog);
+            this.$blogList.insertBefore($blog, this.$blogList.firstChild);
+        },
+        cloneBlogItm: function(blog) {
+            var $blog = this.$blogItmTmp.cloneNode(true);
+            // id
+            var $id = $.querySelector($blog, '.j-id');
+            $id.value = blog.id;
+            // title
+            var $title = $.querySelector($blog, '.j-title');
+            $title.innerHTML = blog.title;
+            // modify time
+            var $date = $.querySelector($blog, '.j-date');
+            $date.innerHTML = zjs.datestr(blog.modifyTime);
+            // read num
+            var $readnum = $.querySelector($blog, '.j-readnum');
+            $readnum.innerHTML = blog.accessCount;
+            // comment num
+            var $cmtnum = $.querySelector($blog, '.j-cmtnum');
+            $cmtnum.innerHTML = blog.commentCount;
+            // show blog
+            $blog.style.display = '';
+            return $blog;
         },
 
         focusBlogTitle: function() {
-            if (this.blogTitle.value === this.defaultBlogTitle) {
-                this.blogTitle.value = '';
+            if (this.$blogTitle.value === this.defaultBlogTitle) {
+                this.$blogTitle.value = '';
             }
         },
         blurBlogTitle: function() {
-            if (this.blogTitle.value === '') {
-                this.blogTitle.value = this.defaultBlogTitle;
+            if (this.$blogTitle.value === '') {
+                this.$blogTitle.value = this.defaultBlogTitle;
             }
         },
         focusBlogCnt: function() {
-            if (this.blogCnt.value === this.defaultBlogCnt) {
-                this.blogCnt.value = '';
+            if (this.$blogContent.value === this.defaultBlogContent) {
+                this.$blogContent.value = '';
             }
         },
         blurBlogCnt: function() {
-            if (this.blogCnt.value === '') {
-                this.blogCnt.value = this.defaultBlogCnt;
+            if (this.$blogContent.value === '') {
+                this.$blogContent.value = this.defaultBlogContent;
             }
         },
         submitBlog: function() {
-            var blogTitle = this.blogTitle.value;
-            var blogCnt = this.blogCnt.value;
+            // 如果是编辑，则将新编辑的日志放置于列表最上方
+            if (!!this.$currBlog) {
+                this.$blogList.insertBefore(this.$currBlog, this.$blogList.firstChild);
+            }
+
+            var id = this.$blogId.value || +new Date;
+            var blogTitle = this.$blogTitle.value;
+            var blogContent = this.$blogContent.value;
             if (!blogTitle || blogTitle === this.defaultBlogTitle) {
                 alert('请填写日志标题');
                 return;
             }
-            if (!blogCnt || blogCnt === this.defaultBlogCnt) {
+            if (!blogContent || blogContent === this.defaultBlogContent) {
                 alert('请填写日志内容');
                 return;
             }
             var blog = {
                 title: blogTitle,
-                cnt: blogCnt
-            };
-            // TODO rest to post the blog
-            this.resetBlog();
-            this.insertBlog(blog);
+                blogContent: blogContent,
+                modifyTime: +new Date,
+                accessCount: 0,
+                commentCount: 0,
+                allowView: -100,
+                classId: 'fks_083069087087080069082083074065092095088064093',
+                id: id,
+                userId: "289939",
+                userName: "force2002",
+                userNickname: "悟空空"
+            }
+            // rest to post the blog
+            var blogStr = '{' + 
+                            '"title":"' + blogTitle + '","blogContent":"' + blogContent + '"}';
+            $.ajax({
+                url: 'http://fed.hz.netease.com/api/addBlog',
+                method: 'POST',
+                data: blogStr,
+                onload: this.cbSubmitBlog.bind(this, blog)
+            });
+        },
+        cbSubmitBlog: function(blog, xhr) {
+            if (xhr.responseText === '1') {
+                // 如果是编辑，则更新，否则新加
+                if (!!this.$currBlog) {
+                    this.blogList[blog.id] = blog;
+                    this.updateModifiedBlog(blog);
+                } else {
+                    this.blogList.splice(0, 0, blog);
+                    this.blogList[blog.id] = blog;
+                    this.insertBlog(blog);
+                }
+                this.resetBlog();
+            }
         },
         resetBlog: function() {
-            this.blogTitle.value = '日志标题';
-            this.blogCnt.value = '这里可以写日志哦~';
+            this.$currBlog = null;
+            this.$blogId.value = '';
+            this.$blogTitle.value = '日志标题';
+            this.$blogContent.value = '这里可以写日志哦~';
+        },
+        updateModifiedBlog: function(blog) {
+            // title
+            var $title = $.querySelector(this.$currBlog, '.j-title');
+            $title.innerHTML = blog.title;
+            // modify time
+            var $date = $.querySelector(this.$currBlog, '.j-date');
+            $date.innerHTML = zjs.datestr(blog.modifyTime);
         },
 
-        insertBlog: function(blog) {
-            var blogItm = this.blogItmTmp.cloneNode(true);
-            console.log(blogItm);
+        clickBlogList: function(event) {
+            var element = event.target|| event.srcElement;
+            this.$currBlog = element.parentNode.parentNode.parentNode;
+            
+            var className = element.className;
+            if (className.indexOf('j-edit') !== -1) {
+                $.stop(event);
+                this.fillBlogForm();
+            } else if (className.indexOf('j-more') !== -1) {
+                $.stop(event);
+                this.showMoreOprts();
+            }
+        },
+        fillBlogForm: function() {
+            var id = $.querySelector(this.$currBlog, '.j-id').value;
+            var blog = this.blogList[id];
+            this.$blogId.value = id;
+            this.$blogTitle.value = blog.title;
+            this.$blogContent.value = blog.blogContent;
+        },
+        showMoreOprts: function() {
+            var $moreOprts = $.querySelector(this.$currBlog, '.j-moreoprts');
+            $moreOprts.style.display = '';
         },
 
         checkAllBlog: function() {
--- js/zjs.js
+++ js/zjs.js
@@ -26,6 +26,85 @@
         }
         return elem;
     };
+    //支持 #id|.classname|tagname|tagname.classname|tagname#id四种，如果要多层筛选，请重复调用
+    zjs.querySelectorAll = function(node,selector){
+        if(typeof(document.querySelectorAll)!='undefined'){
+            return node.querySelectorAll(selector);
+        }
+        var tname;
+        var citems;
+        function findElementsByClassNameInner(n,className){
+            var items= n.children;
+            for(var i=0;i<items.length;i++){
+                if(items[i].className==className){
+                    citems.push(items[i]);
+                }
+                findElementsByClassNameInner(items[i],className);
+            }
+            return citems;
+        }
+        function findElementsByIDInner(n,id){
+            var items= n.children;
+            for(var i=0;i<items.length;i++){
+                if(items[i].id==id){
+                    citems.push(items[i]);
+                }
+                findElementsByIDInner(items[i],id);
+            }
+            return citems;
+        }
+        if(selector.charAt(0)=='.'){
+            tname=selector.substring(1);
+            citems=new Array();
+            var items=findElementsByClassNameInner(node,tname);
+            return items;
+        }
+        else if(selector.charAt(0)=='#'){
+            var id=selector.substring(1);
+            citems=new Array();
+            var items=findElementsByIDInner(node,id);
+            return items;
+        }
+        else if((r=selector.match(/(\w+)\.(\w+)/))!=null){
+            tagName=r[1].toLowerCase();
+            className=r[2];
+            citems=new Array();
+            var titems=findElementsByClassNameInner(node,className);
+            var items=new Array();
+            for(i=0;i<titems.length;i++){
+                if(titems[i].tagName && titems[i].tagName.toLowerCase()==tagName){
+                    items.push(titems[i]);
+                }
+            }
+            return items;
+        }
+        else if((r=selector.match(/(\w+)#(\w+)/))!=null){
+            tagName=r[1].toLowerCase();
+            id=r[2];
+            citems=new Array();
+            var titems=findElementsByIDInner(node,id);
+            var items=new Array();
+            for(i=0;i<titems.length;i++){
+                if(titems[i].tagName && titems[i].tagName.toLowerCase()==tagName){
+                    items.push(titems[i]);
+                }
+            }
+            return items;
+        }
+        else{
+            var items= node.getElementsByTagName(selector);
+            return items;
+        }
+    };
+    //支持 #id|.classname|tagname|tagname.classname|tagname#id四种，如果要多层筛选，请重复调用
+    zjs.querySelector = function(node,selector){
+        if(node==null) return null;
+        if(typeof(document.querySelector)!='undefined'){
+            return node.querySelector(selector);
+        }
+        var items = this.querySelectorAll(node,selector);
+        return items.length==0 ? null:items[0]
+    };
 
     zjs.addEvent = function(elem, type, handler) {
         if (elem.addEventListener) {
@@ -59,7 +138,7 @@
             method = options.method.toUpperCase(),
             async = options.async || true,
             data = options.data,
-            callback = options.callback || _f;
+            onload = options.onload || _f;
 
         if (method !== 'GET' && method !== 'POST') {
             method = 'GET';
@@ -74,29 +153,52 @@
         }
         xhr.onreadystatechange = function() {
             if (xhr.readyState === 4 && xhr.status === 200) {
-                callback(xhr);
+                onload(xhr);
             }
         };
         xhr.open(method, url, async);
         if (method === 'GET') {
             xhr.send();
         } else {
-            xhr.send(JSON.stringify(data));
+            xhr.send(data);
         }
     };
 
     zjs.rest = function(options) {
-        var callback = options.callback || _f;
-        options.callback = function(xhr) {
+        var onload = options.onload || _f;
+        options.onload = function(xhr) {
             var r = xhr.responseText
-            callback(JSON.parse(r));
+            onload(!!JSON ? JSON.parse(r) : eval(r));
         };
         this.ajax(options);
+    };
+
+    zjs.datestr = function(date) {
+        date = new Date(+date);
+        var year = date.getFullYear(),
+            month = date.getMonth() + 1,
+            day = date.getDate(),
+            hour = date.getHours(),
+            minute = date.getMinutes();
+        if (month < 10) {
+            month = '0' + month;
+        }
+        if (day < 10) {
+            day = '0' + day;
+        }
+        if (hour < 10) {
+            hour = '0' + hour;
+        }
+        if (minute < 10) {
+            minute = '0' + minute;
+        }
+        return year + '-' + month + '-' + day + ' ' + hour + ':' + minute;
     };
 
     window.zjs = window.$ = zjs;
 })();
 
+// Function.prototype extension
 (function() {
     var _o = {},
         _r = [],
@@ -111,16 +213,5 @@
                 return func.apply(obj, args);
             };
         };
-    }
-})();
-
-// Create a JSON object only if one does not already exist. We create the
-// methods in a closure to avoid creating global variables.
-if (typeof JSON !== 'object') {
-    JSON = {};
-}
-(function() {
-    if (typeof JSON.stringify !== 'function') {
-        JSON.stringify = function() {};
     }
 })();
